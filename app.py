4cc112fc23f9c1156cbad8d013a516194333a604
emna <emna.gaidii@gmail.com>
2024-11-06 23:02:13 +0100

    unused function added

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ def add_numbers(a, b):
     return a + b
 
 
+# Introducing an unused function, which SonarCloud should flag
+def unused_function():
+    x = "This function does nothing"
+    print(x)
+
+
+# Introducing a variable with a single letter (SonarCloud prefers descriptive names)
 if __name__ == "__main__":
     result = add_numbers(5, 7)
     print(f"Le résultat de l'addition est : {result}")
