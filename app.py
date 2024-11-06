824663a1ab0c13015501fcf9bd4852a4f96a719e
emna <emna.gaidii@gmail.com>
2024-11-06 22:22:50 +0100

    first

---
+++ app.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    """Additionne deux nombres et retourne le résultat."""
+    return a + b
+
+
+if __name__ == "__main__":
+    result = add_numbers(5, 7)
+    print(f"Le résultat de l'addition est : {result}")
